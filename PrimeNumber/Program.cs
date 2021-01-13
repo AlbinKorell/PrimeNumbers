e82b23a1d4f81d69c2f9a3f0b40bd1747d2dcef1
unknown <albin.korell@gmail.com>
2021-01-13 12:42:41 +0100

    choose number to test Prime

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uTal;
+            int i;
+            int hTal = 0; 
+            int prim = 0;
+
+            Console.Write("Skriv ett tal: ");
+            uTal = int.Parse(Console.ReadLine());
+
+            hTal = uTal / 2;
+
+            for (i = 2 ; i <= hTal ; i++)
+            {
+                if (uTal % i == 0)
+                {
+                    prim = 1;
+
+                    Console.Write(uTal + " är inte ett primtal.");
+                    break;
+                }
+            }
+
+            if (prim == 0)
+                Console.Write(uTal + " är ett primtal.");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
